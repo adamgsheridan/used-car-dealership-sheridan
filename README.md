d1ecb67f9faa3395cc6f440993495bf391b21b09
adamgsheridan <adamgsheridan7@gmail.com>
2026-07-16 00:05:31 -0400

    Initial Express project set up

---
+++ README.md
@@ -0,0 +1,15 @@
+Project Description:
+This site was made for the purpose of helping current or future clients of Sherdog's Used Car Dealership find the car they need.
+
+Database Schema:
+[image of ERD exported from pgAdmin showing the tables and relationships]
+
+User Roles:
+Admin:
+User:
+
+Test Account Credentials:
+[Username or email for one account of each role type, do not include the password in the README, but use P@$$w0rd! for all users]
+
+Known Limitations:
+Site is not yet finished.
---
+++ server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "src/views"));
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://127.0.0.1:${PORT}`);
+});
+
