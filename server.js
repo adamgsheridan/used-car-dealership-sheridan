e2708d6f90f8bc518a4931cd826573d2bc00c745
adamgsheridan <adamgsheridan7@gmail.com>
2026-07-18 13:41:46 -0400

    Header, footer, nav, and basic css done and working.

--- server.js
+++ server.js
@@ -13,11 +13,36 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "src/views"));
 
+// Home page
 app.get("/", (req, res) => {
     res.render("home");
+});
+
+// Inventory Page
+app.get("/inventory", (req, res) => {
+    res.render("inventory");
+});
+
+// Financing Page
+app.get("/financing", (req, res) => {
+    res.render("financing");
+});
+
+// Trade In Page
+app.get("/tradein", (req, res) => {
+    res.render("tradein");
+});
+
+// About Page
+app.get("/about", (req, res) => {
+    res.render("about");
+});
+
+// Contact Page
+app.get("/contact", (req, res) => {
+    res.render("contact");
 });
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://127.0.0.1:${PORT}`);
 });
-
